dff9d4bea3c9a0d7f9b431f3ffb8f7e1dfcb8004
TreyKockelman <kockelmant@gmail.com>
2022-09-22 17:55:01 -0500

    fixed quiz

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,6 +28,30 @@ var questions = [
   choices: ["Jones", "Kockelman", "Brooks", "Mickelson"],
   answer: 1,
   },
+
+  {
+  question: "What is my dogs name?",
+  choices: ["Jeter", "Stinky", "Boots", "Ace"],
+  answer: 0,
+  },
+
+  {
+  question: "Who is the best coding teacher?",
+  choices: ["Julia", "Will", "Aaron", "Gary"],
+  answer: 3,
+  },
+
+  {
+  question: "What do you use to add dynamic elements?",
+  choices: ["HTML", "CSS", "JavaScript", "Not Sure"],
+  answer: 2,
+  },
+
+  {
+  question: "Who is the best coding student?",
+  choices: ["Trey", "Kayla", "Will", "Ryan"],
+  answer: 2,
+  },
 ]
 
 
@@ -40,6 +64,8 @@ function startQuiz() {
     timerText.textContent = "Timer: " + secondsRemaining + " seconds remaining"; // sets the content of the text on the timer to whatever is left
     if (secondsRemaining === 0) {
       clearInterval(timerInterval); // clears interval when timer hits zero
+      document.getElementById("question-holder").style.display = "none";
+      storeHighScore();
     } 
   }, 1000); // sets interval tick to 1 second
   displayQuestions(questions[questionIndex]);
@@ -58,9 +84,12 @@ function clearElement(element) {
 }
 
 function addElements(arr, parentElement) {
+  var header = document.createElement("h2");
+  header.textContent = arr.question
+  parentElement.appendChild(header);
+
   for (var i = 0; i < arr.choices.length; i++) {
     var element = document.createElement("button");
-    console.log(i);
 
     if (i == arr.answer) {
       element.setAttribute("data-ans", "yes");
@@ -90,11 +119,16 @@ function hasQuestions() {
 function storeHighScore() {
   var highScore = 0;
   var initials = "";
-  localStorage.setItem("highScore", highScore);
+  localStorage.setItem("highScore", secondsRemaining);
   localStorage.setItem("initials", initials);
 }
 
 // Function for high score display
+function displayHighScore() {
+  document.getElementById("question-holder").style.display = "none";
+  clearInterval(timerInterval);
+
+}
 
 
 
@@ -102,7 +136,7 @@ function storeHighScore() {
 startButton.addEventListener("click", function () {
   document.getElementById("start-button").style.display = "none";
   startQuiz();
-})
+});
 
 // listening for button click on answer to questions
 buttonHolder.addEventListener("click", function (evt) {
@@ -118,6 +152,8 @@ buttonHolder.addEventListener("click", function (evt) {
       displayQuestions(questions[questionIndex]);
     } else {
       console.log("quiz over");
+      displayHighScore();
+      storeHighScore();
     }
   }
-})
+});
